da8e5aff4e2bf330940928a9b8ffb8389ad6ec94
bui.van.hieu <FRAMGIA\bui.van.hieu@b121668-pc.framgia.com>
2019-03-07 17:26:53 +0700

    add searchview

--- app/src/main/java/com/example/androidadvancedemo/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidadvancedemo/ui/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.androidadvancedemo.ui
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -13,8 +20,9 @@ import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class MainActivity : DaggerAppCompatActivity(), ItemCLickListener {
+class MainActivity : DaggerAppCompatActivity(), ItemCLickListener{
     private lateinit var viewModel: MainViewModel
+    private var searchView: SearchView? = null
     private var userAdapter = UserAdapter(this)
 
     @Inject
@@ -27,9 +35,12 @@ class MainActivity : DaggerAppCompatActivity(), ItemCLickListener {
         initData()
     }
 
-    private fun setupView(){
+    private fun setupView() {
         val dividerItemDecoration = DividerItemDecoration(recyclerView.context, LinearLayoutManager.VERTICAL)
         recyclerView.addItemDecoration(dividerItemDecoration)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        supportActionBar!!.title = "filter"
     }
 
     private fun initData() {
@@ -41,5 +52,36 @@ class MainActivity : DaggerAppCompatActivity(), ItemCLickListener {
 
     override fun onItemClicked(user: User) {
         Toast.makeText(this, user.name, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_menu, menu)
+//        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+//        searchView = menu!!.findItem(R.id.action_search).actionView as SearchView
+//        searchView!!.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+//        searchView!!.maxWidth = Int.MAX_VALUE
+//        searchView!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.action_search -> {
+            }
+            R.id.action_online -> {
+            }
+            R.id.action_offline -> {
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
